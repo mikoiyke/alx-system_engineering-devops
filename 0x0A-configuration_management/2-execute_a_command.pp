f46388b1256d423cffebb3896fafff05ec469e3f
mikoiyke <mikoiyke@gmail.com>
2024-04-23 08:50:45 -0700

    puppet lint errors fixed

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,8 +1,8 @@
 # Puppet Manifest: Kill the 'killmenow' process
 
 exec { 'kill_killmenow_process':
-  command     => '/bin/pkill killmenow',
-  onlyif      => '/bin/pgrep killmenow',
-  path        => ['/usr/bin'],
-  user        => 'root',
+  command => '/bin/pkill killmenow',
+  onlyif  => '/bin/pgrep killmenow',
+  path    => ['/usr/bin'],
+  user    => 'root',
 }
