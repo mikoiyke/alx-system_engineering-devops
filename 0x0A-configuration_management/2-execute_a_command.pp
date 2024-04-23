f1792f0ac20a6116dc2b6c00923990afe125cff6
mikoiyke <mikoiyke@gmail.com>
2024-04-23 08:34:24 -0700

    2-execute_a_command.pp

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,8 +1,8 @@
 # Puppet Manifest: Kill the 'killmenow' process
 
 exec { 'kill_killmenow_process':
-  command     => '/bin/killmenow $(/bin/pgrep killmenow)',
+  command     => '/bin/pkill killmenow',
   onlyif      => '/bin/pgrep killmenow',
-  path        => ['/bin', '/usr/bin'],
+  path        => ['/usr/bin'],
   user        => 'root',
 }
